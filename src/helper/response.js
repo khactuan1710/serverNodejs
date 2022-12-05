5cee5bf6dc5fae1ae39ffcc8d83f8a060ea3baa6
khactuan1710 <khactuan1710@gmail.com>
2022-12-05 19:09:58 +0700

    fix bugs

--- src/helper/response.js
+++ src/helper/response.js
@@ -31,7 +31,7 @@ exports.ErrorResponse = function (res, msg) {
         status: 0,
         message: msg,
     };
-    return res.status(500).json(data);
+    return res.status(200).json(data);
 };
 
 exports.notFoundResponse = function (res, msg) {
