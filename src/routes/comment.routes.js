7208bf9888155ef0b9f711a43650d89703055f74
NguyenKhacTuan <@tts.tuannk>
2022-12-06 16:24:23 +0700

    fix bugs

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const CommentController = require("../controller/comment.controller");
 
 router.post("/addComment", CommentController.addComment);
-router.get("/getCommentById", CommentController.getCommentById);
+router.post("/getCommentById", CommentController.getCommentById);
 
 module.exports = router;
